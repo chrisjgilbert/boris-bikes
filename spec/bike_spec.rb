cc41f9c1b629dc9bf08a243c947e53af4bec5491
Chris Gilbert <chrisgilbert4@icloud.com>
2018-11-01 15:11:46 +0000

    working? test moved to bike_spec and docking_sation dock test refactored

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,7 +3,7 @@ require 'bike'
 describe Bike do
   it { is_expected.to respond_to :working? }
 
-  it 'bikes are working' do
+  it 'the bike should be working' do
     bike = Bike.new
     expect(bike).to be_working
   end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,17 +4,11 @@ describe DockingStation do
 
   it { is_expected.to respond_to :release_bike }
 
-  it 'it returns a working bike' do
-    bike = Bike.new
-    expect(bike).to be_working
-  end
-
   it { is_expected.to respond_to(:dock).with(1).argument }
 
   it 'allows user to see if a bike has been docked' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
+    subject.dock("BMC")
+    expect(subject.bike).to eq "BMC"
   end
 
   describe "#release_bike" do
