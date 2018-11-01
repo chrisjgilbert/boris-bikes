1916ea7d4506a3b287e52f2bdea0ac6ed455f1ae
Chris Gilbert <chrisgilbert4@icloud.com>
2018-11-01 08:20:30 +0000

    dock method implemented and tested (11)

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,21 @@
 require 'docking_station'
 
 describe DockingStation do
+  
   it { is_expected.to respond_to :release_bike }
 
   it 'it returns a working bike' do
     bike = subject.release_bike
     expect(bike).to be_working
+  end
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+
+  it 'allows user to see if a bike has been docked' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
   end
 
 end
