f5cd9d8ec65888866fcd2129d9e7754c8dbe5390
Chris Gilbert <chrisgilbert4@icloud.com>
2018-11-02 09:50:41 +0000

    refactored and removed older redundant tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,27 +2,25 @@ require 'docking_station'
 
 describe DockingStation do
 
-  it { is_expected.to respond_to :release_bike }
-
-  it { is_expected.to respond_to(:dock).with(1).argument }
+  let(:dockingstation) { DockingStation.new }
 
   describe "#release_bike" do
     it "return error if station is empty" do
-      expect {subject.release_bike}.to raise_error "Station is empty"
+      expect {dockingstation.release_bike}.to raise_error "Station is empty"
     end
   end
 
   describe "#dock" do
     it "returns an error when the bike rack is full" do
-      20.times { subject.dock(Bike.new) }
-      expect {subject.dock("BMC")}.to raise_error "Station is full"
+      20.times { dockingstation.dock(Bike.new) }
+      expect {dockingstation.dock("BMC")}.to raise_error "Station is full"
     end
   end
 
   it 'allows user to see if a bike has been docked' do
     bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike_rack.include?(bike)).to eq true
+    dockingstation.dock(bike)
+    expect(dockingstation.bike_rack.include?(bike)).to eq true
   end
 
 end
