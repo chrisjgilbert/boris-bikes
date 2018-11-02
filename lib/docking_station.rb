e4589c480f0e2356180122595c8b2c9d0bc19edf
Chris Gilbert <chrisgilbert4@icloud.com>
2018-11-02 14:29:58 +0000

    refactored

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -10,19 +10,19 @@ class DockingStation
   end
 
   def release_bike
-    fail "Station is empty" if bike_rack.empty?
-    bike_rack.pop
+    fail "Station is empty" if @bike_rack.empty?
+    @bike_rack.pop
   end
 
   def dock(bike)
     fail "Station is full" if full?
-    bike_rack << bike
+    @bike_rack << bike
   end
 
   private
 
   def full?
-     bike_rack.size >=  DEFAULT_CAPACITY
+     @bike_rack.size >=  DEFAULT_CAPACITY
   end
 
 end
